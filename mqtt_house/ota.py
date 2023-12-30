ffc405866edefd93cfc50cd28d82990f396997d5
Mark Hall <mark.hall@work.room3b.eu>
2023-12-30 11:27:15 +0000

    Cleaned up the OTA client

--- mqtt_house/ota.py
+++ mqtt_house/ota.py
@@ -4,66 +4,80 @@ from importlib import resources
 from json import dumps
 from time import sleep
 
-from httpx import Client, Response, TransportError, codes
+from httpx import Client, TransportError, codes
 from rich.progress import Progress
 from typer import FileBinaryRead
 from yaml import safe_load
 
-from mqtt_house.__about__ import __version__
 from mqtt_house.base import app, console
 from mqtt_house.settings import ConfigModel
 from mqtt_house.util import slugify
 
 
-def get_device_version(config: ConfigModel, client: Client, progress: Progress) -> dict | None:
+class OTAError(Exception):
+    """An exception raised during the OTA update."""
+
+    pass
+
+
+def get_device_version(config: ConfigModel, client: Client) -> dict | None:
     """Retrieves the device's version or None if the device could not be contacted."""
-    task = progress.add_task("Checking the device version", total=1, start=False)
     try:
         response = client.get(f"http://{slugify(config.device.name)}.{config.device.domain}/ota/about")
         if response.status_code == codes.OK:
-            return response.json()
-    except TransportError:
-        pass
-    finally:
-        progress.start_task(task)
-        progress.update(task, advance=1)
-    return None
-
-
-def upload_data(config: ConfigModel, client: Client, item: dict) -> Response:
-    sha256_hash = sha256(item["data"])
-    return client.put(
-        f"http://{slugify(config.device.name)}.{config.device.domain}/ota/file",
-        content=item["data"],
-        headers={"X-Fileid": item["fileid"], "X-Filehash": sha256_hash.hexdigest()},
-    )
-
-
-def perform_upload(config: ConfigModel, client: Client, progress: Progress) -> str:
-    """Perform the actual upload of the configuration.
-
-    This checks that the device can receive the configuration, uploads it, resets the device, and then waits for the
-    device to become available again.
-    """
-    # Check that the device is connectable and has a version that is supported.
-    about = get_device_version(config, client, progress)
-    if about is None:
-        return (
-            f":x: [logging.level.error]The device could not be contacted at http://{slugify(config.device.name)}."
-            "{config.device.domain}. Please check the device and domain name settings."
+            return [int(c) for c in response.json()["version"].split(".")]
+        msg = (
+            f"Failed to get the device version from http://{slugify(config.device.name)}.{config.device.domain} "
+            "({response.status_code})."
         )
-    elif "version" not in about:
-        return (
-            f":x: [logging.level.error]The device at http://{slugify(config.device.name)}.{config.device.domain} "
-            "did not respond with version information. Please check the device and domain name settings."
+
+        raise OTAError(msg)
+    except TransportError as err:
+        msg = f"The device could not be reached at http://{slugify(config.device.name)}.{config.device.domain}."
+        raise OTAError(msg) from err
+    except KeyError as err:
+        msg = f"Failed to get a valid device version from http://{slugify(config.device.name)}.{config.device.domain}."
+        raise OTAError(msg) from err
+
+
+def prepare_device(config: ConfigModel, client: Client, progress: Progress) -> None:
+    """Prepare the device for the OTA update."""
+    task = progress.add_task("Preparing the device", total=2)
+    # Check the device version allows an upgrade
+    version = get_device_version(config, client)
+    if version[0] != 0:
+        msg = (
+            f"The device at http://{slugify(config.device.name)}.{config.device.domain} "
+            "has a major version mismatch and cannot be updated over-the-air."
         )
-    elif about["version"] != __version__:
-        return (
-            f":x: [logging.level.error]The device at http://{slugify(config.device.name)}.{config.device.domain} "
-            "did not respond with the current version. Please update the device first."
-        )
+        raise OTAError(msg)
+    progress.update(task, advance=1)
+    # Rollback any existing upgrade
+    try:
+        response = client.post(f"http://{slugify(config.device.name)}.{config.device.domain}/ota/rollback")
+        if response.status_code != codes.NO_CONTENT:
+            msg = (
+                f"Preparing the device at http://{slugify(config.device.name)}.{config.device.domain} "
+                f"for the update failed ({response.status_code})"
+            )
+            raise OTAError(msg)
+    except TransportError as err:
+        msg = f"The device could not be reached at http://{slugify(config.device.name)}.{config.device.domain}."
+        raise OTAError(msg) from err
+    progress.update(task, advance=1)
+
+
+def prepare_update(config: ConfigModel) -> tuple[list, list]:
+    """Prepare the inventory and files for upload."""
+    inventory = []
     files = []
-    # Prepare the files to upload
+    # Add the config files
+    inventory.append(
+        {
+            "fileid": "1",
+            "filename": "config.json",
+        }
+    )
     files.append(
         {
             "fileid": "1",
@@ -77,6 +91,7 @@ def perform_upload(config: ConfigModel, client: Client, progress: Progress) -> s
             ).encode(),
         }
     )
+    inventory.append({"fileid": "2", "filename": "entities.json"})
     files.append({"fileid": "2", "filename": "entities.json", "data": dumps(config.entities).encode()})
     base_path = resources.files("mqtt_house.micro")
     for idx, filename in enumerate(
@@ -92,38 +107,62 @@ def perform_upload(config: ConfigModel, client: Client, progress: Progress) -> s
         item_file = base_path
         for part in filename.split("/"):
             item_file = item_file / part
+        inventory.append({"fileid": str(idx + 3), "filename": filename})
         files.append({"fileid": str(idx + 3), "filename": filename, "data": item_file.read_bytes()})
-    # Upload the inventory
-    task = progress.add_task("Uploading the inventory", total=1)
-    inventory = []
+    return inventory, files
+
+
+def upload_file(item: dict, config: ConfigModel, client: Client, endpoint: str = "/file") -> None:
+    """Upload a single file to the device."""
+    sha256_hash = sha256(item["data"])
+    try:
+        response = client.put(
+            f"http://{slugify(config.device.name)}.{config.device.domain}/ota{endpoint}",
+            content=item["data"],
+            headers={"X-Fileid": item["fileid"], "X-Filehash": sha256_hash.hexdigest()},
+        )
+        if response.status_code != codes.NO_CONTENT:
+            msg = (
+                f"Failed to upload {item['filename']} to http://{slugify(config.device.name)}.{config.device.domain}"
+                f" ({response.status_code})."
+            )
+            raise OTAError(msg)
+    except TransportError as err:
+        msg = f"The device could not be reached at http://{slugify(config.device.name)}.{config.device.domain}."
+        raise OTAError(msg) from err
+
+
+def upload_files(inventory: list, files: list, config: ConfigModel, client: Client, progress: Progress) -> None:
+    """Upload all files to the device."""
+    task = progress.add_task("Uploading the new files", total=len(files) + 1)
+    upload_file({"fileid": "-1", "data": dumps(inventory).encode()}, config, client, endpoint="/inventory")
+    progress.update(task, advance=1)
     for item in files:
-        inventory.append({"fileid": item["fileid"], "filename": item["filename"]})
-    response = client.put(
-        f"http://{slugify(config.device.name)}.{config.device.domain}/ota/inventory",
-        content=dumps(inventory).encode(),
-        headers={"X-Filehash": sha256(dumps(inventory).encode()).hexdigest()},
-    )
-    if response.status_code == codes.NO_CONTENT:
+        upload_file(item, config, client)
         progress.update(task, advance=1)
-    else:
-        return f":x: [logging.level.error]Received error {response.status_code} when uploading the inventory"
-    # Upload the files
-    task = progress.add_task("Uploading the files", total=len(files))
-    for item in files:
-        response = upload_data(config, client, item)
-        if response.status_code == codes.NO_CONTENT:
-            progress.update(task, advance=1)
-        else:
-            return f":x: [logging.level.error]Received error {response.status_code} when uploading the {filename}."
-    # Start the update
+
+
+def commit_update(config: ConfigModel, client: Client, progress: Progress) -> None:
+    """Commit all uploaded changes."""
     task = progress.add_task("Committing the changes", total=1, start=False)
-    response = client.post(f"http://{slugify(config.device.name)}.{config.device.domain}/ota/commit")
-    progress.start_task(task)
-    if response.status_code == codes.NO_CONTENT:
-        progress.update(task, completed=1)
-    else:
-        return f":x: [logging.level.error]Received error {response.status_code} when committing the update."
-    # Reset the device and wait for it to become available again.
+    try:
+        response = client.post(f"http://{slugify(config.device.name)}.{config.device.domain}/ota/commit")
+        progress.start_task(task)
+        if response.status_code == codes.NO_CONTENT:
+            progress.update(task, completed=1)
+        else:
+            msg = (
+                f"The update to http://{slugify(config.device.name)}.{config.device.domain} failed to"
+                f" commit correctly ({response.status_code})."
+            )
+            raise OTAError(msg)
+    except TransportError as err:
+        msg = f"The device could not be reached at http://{slugify(config.device.name)}.{config.device.domain}."
+        raise OTAError(msg) from err
+
+
+def reset(config: ConfigModel, client: Client, progress: Progress) -> None:
+    """Reset the device and wait for it to reappear."""
     task = progress.add_task("Resetting the device", total=60, start=False)
     response = client.post(
         f"http://{slugify(config.device.name)}.{config.device.domain}/ota/reset",
@@ -146,23 +185,35 @@ def perform_upload(config: ConfigModel, client: Client, progress: Progress) -> s
                 countdown = countdown - 1
                 progress.update(task, advance=1)
         progress.update(task, completed=60)
-        if success:
-            return ":heavy_check_mark: [green]The device has been updated with the new configuration."
-        else:
-            return (
-                f":x: [logging.level.error]The device at http://{slugify(config.device.name)}.{config.device.domain}"
+        if not success:
+            msg = (
+                f"The device at http://{slugify(config.device.name)}.{config.device.domain}"
                 " did not reappear within one minute after resetting."
             )
+            raise OTAError(msg)
     else:
-        progress.update(task, completed=60)
-        return ":x: [logging.level.error]Received error {response.status_code} when trying to reset the device."
+        msg = (
+            f"Received error {response.status_code} when trying to reset the device at"
+            f" http://{slugify(config.device.name)}.{config.device.domain}."
+        )
+        raise OTAError(msg)
 
 
 @app.command()
 def ota_deploy(config_file: FileBinaryRead):
     """Deploy a device via an OTA update."""
     config = ConfigModel(**safe_load(config_file))
-    with Client() as client:
-        with Progress() as progress:
-            result = perform_upload(config, client, progress)
-    console(result)
+    try:
+        with Client() as client:
+            with Progress() as progress:
+                prepare_device(config, client, progress)
+                upload_files(*prepare_update(config), config, client, progress)
+                commit_update(config, client, progress)
+                reset(config, client, progress)
+                console(
+                    f":heavy_check_mark: [green]The device http://{slugify(config.device.name)}.{config.device.domain}"
+                    " has been updated with the new configuration."
+                )
+    except OTAError as e:
+        console(f":x: [logging.level.error]{e!s}")
+    # console(result)
