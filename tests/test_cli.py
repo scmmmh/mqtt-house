f55b0c35a24e6ffa14cd3670933d0b841615835b
Mark Hall <mark.hall@work.room3b.eu>
2023-12-31 11:37:37 +0000

    Added some basic tests

---
+++ tests/test_cli.py
@@ -0,0 +1,20 @@
+"""Test the basic CLI functionality."""
+from typer.testing import CliRunner
+
+from mqtt_house.__about__ import __version__
+from mqtt_house.cli import app
+
+runner = CliRunner()
+
+
+def test_no_command_fails():
+    """Test that running with no command fails."""
+    result = runner.invoke(app, [])
+    assert result.exit_code == 2
+
+
+def test_version():
+    """Test that running the version command returns the current version."""
+    result = runner.invoke(app, ["version"])
+    assert result.exit_code == 0
+    assert __version__ in result.stdout
